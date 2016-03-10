6e19d1841ef657735f8a563bebcc69bb196aa185
isethguy <isethguy@gmail.com>
2016-03-09 21:52:27 -0500

    can i fit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var url = 'http://'+ip+':'+port+''+mountPath;
 
 //var databaseUri  = 'mongodb://localhost:27017/ngreen';
 
-var databaseUri = 'mongodb://admin:sVQazydEBrY8@127.13.21.2:27017/ngreen';
+var databaseUri = 'mongodb://admin:SLIQk4Kja2Tn@127.4.226.2:27017/gflex';
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
