9b7e5b58599bb1566ddce79305e7f47fd9c4b906
isethguy <isethguy@gmail.com>
2016-03-12 15:43:45 -0500

    flop

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ var mountPath = '/parse';
 
 var url = 'http://'+ip+':'+port+''+mountPath; 
 
-var databaseUri  =  process.env.OPENSHIFT_MONGODB_DB_URL+"glex"  ||'mongodb://localhost:27017/ngreen';
+//var databaseUri  =  process.env.OPENSHIFT_MONGODB_DB_URL+"glex"  || 'mongodb://localhost:27017/ngreen';
 
-//var databaseUri =  'mongodb://admin:SLIQk4Kja2Tn@127.4.226.2:27017/gflex';
+var databaseUri =  'mongodb://admin:SLIQk4Kja2Tn@127.4.226.2:27017/gflex';
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
