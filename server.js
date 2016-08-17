496c822c00e664a14445b2b11062bb8255e7777b
isethguy <isethguy@gmail.com>
2016-08-16 21:39:37 -0400

    small config fixes

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ var Signupurl = relLink + "?signup";
 //db.auth('admin','SLIQk4Kja2Tn');
 
 
-var databaseUri = 'mongodb://127.0.0.1:27017/gflex';
+//var databaseUri = 'mongodb://127.0.0.1:27017/gflex';
 
-//var databaseUri = 'mongodb://127.4.226.2:27017/gflex';
+var databaseUri = 'mongodb://127.4.226.2:27017/gflex';
 if (!databaseUri) {
     console.log('DATABASE_URI not specified, falling back to localhost.');
 }
